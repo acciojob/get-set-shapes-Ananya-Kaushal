4ecc661deab02b0d69a1bc4990567d237ebf1ead
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2026-02-10 22:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 //complete this code
 class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-	constructor(width,height)
-	{
-		this._width=width;
-		this._height=height;
-	}
+  // Use the 'get' keyword to create getter methods
+  get width() {
+    return this._width;
+  }
 
-	getWidth()
-	{
-		return this._width;
-	}
+  get height() {
+    return this._height;
+  }
 
-	getHeight()
-	{
-		return this._height;
-	}
-
-	getArea()
-	{
-		return this._width*this._height;
-	}
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
-class Square extends Animal {
+// Square inherits from Rectangle
+class Square extends Rectangle {
+  constructor(side) {
+    // Call the parent constructor with side for both width and height
+    super(side, side);
+  }
 
-	constructor(side)
-	{
-		super(side,side);
-	}
-
-	getPerimenter()
-	{
-		return 4*this.width; 
-	}
-
-	
+  getPerimeter() {
+    // You can use this.width (the getter) or this._width
+    return 4 * this.width;
+  }
 }
 
 // Do not change the code below this line
