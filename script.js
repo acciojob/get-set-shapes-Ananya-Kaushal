0168a28a8130b951fc2bbc36b13586c96d812066
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2026-02-10 20:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
 
-class Square extends Animal {}
+	construct(width,height)
+	{
+		this.width=width;
+		this.height=height;
+	}
+
+	getWidth()
+	{
+		return this.width;
+	}
+
+	getHeight()
+	{
+		return this.height;
+	}
+
+	getArea()
+	{
+		return this.width*this.height;
+	}
+}
+
+class Square extends Animal {
+
+	construct(side)
+	{
+		super(side,side);
+	}
+
+	getPerimenter()
+	{
+		return 4*this.side; 
+	}
+
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
